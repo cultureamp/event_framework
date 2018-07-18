abdc1a1056afc90548eb5da6e88093cc81b0aada
Odin Dutton <odindutton@gmail.com>
2018-07-19 09:50:20 +1000

    Give events.uuid a default value

--- db/migrations/20180716073810_create_events.rb
+++ db/migrations/20180716073810_create_events.rb
@@ -1,8 +1,10 @@
 Sequel.migration do
   change do
+    run 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp"'
+
     create_table(:events) do
       column :sequence_id, :bigserial, primary_key: true
-      column :id, :uuid, unique: true, null: false
+      column :id, :uuid, unique: true, null: false, default: Sequel.lit('uuid_generate_v4()')
       column :aggregate_id, :uuid, null: false
       column :type, :varchar, null: false
       column :body, :json, null: false
