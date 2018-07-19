be626e86f778fbc65d289318b264407f71fc10eb
Odin Dutton <odindutton@gmail.com>
2018-07-19 15:08:30 +1000

    Extract event body serializer

--- lib/event_store/sink.rb
+++ lib/event_store/sink.rb
@@ -5,21 +5,24 @@ module EventFramework
   class EventStore
     class Sink
       ConcurrencyError = Class.new(Error)
+      EventBodySerializer = -> (event) {
+        event.to_h.reject do |k, v|
+          %i[
+            aggregate_id
+            aggregate_sequence_id
+          ].include?(k)
+        end
+      }
 
       def self.sink(aggregate_id:, events:)
         database.transaction do
           events.each do |event|
-            body = event.to_h.tap do |h|
-              h.delete :aggregate_id
-              h.delete :aggregate_sequence_id
-            end
-
             begin
               database[:events].insert(
                 aggregate_id: aggregate_id,
                 aggregate_sequence_id: event.aggregate_sequence_id,
                 type: event.class.name,
-                body: Sequel.pg_jsonb(body)
+                body: Sequel.pg_jsonb(EventBodySerializer.call(event))
               )
             rescue Sequel::UniqueConstraintViolation
               raise ConcurrencyError,
