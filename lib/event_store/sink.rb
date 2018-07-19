f9c114a0dd160dddd36b3dee78686890514f2a95
Odin Dutton <odindutton@gmail.com>
2018-07-19 10:51:31 +1000

    Extract method

--- lib/event_store/sink.rb
+++ lib/event_store/sink.rb
@@ -6,12 +6,17 @@ module EventFramework
     class Sink
       def self.sink(aggregate_id:, events:)
         events.each do |event|
-          EventStore.database[:events].insert(
+          database[:events].insert(
             aggregate_id: aggregate_id,
             type: event.class.name,
             body: Sequel.pg_jsonb(event.to_h.tap { |h| h.delete :aggregate_id })
           )
         end
+      end
+
+      private_class_method \
+      def self.database
+        EventStore.database
       end
     end
   end
