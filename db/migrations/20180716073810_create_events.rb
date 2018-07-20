b3d9ee9e16f0790e616c31cff1fe34866cecaf23
Odin Dutton <odindutton@gmail.com>
2018-07-20 17:42:12 +1000

    Rename sequence and aggregate_sequence

--- db/migrations/20180716073810_create_events.rb
+++ db/migrations/20180716073810_create_events.rb
@@ -3,15 +3,15 @@ Sequel.migration do
     run 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp"'
 
     create_table :events do
-      column :sequence_id, :bigserial, primary_key: true
-      column :aggregate_sequence_id, :bigint, null: false
+      column :sequence, :bigserial, primary_key: true
+      column :aggregate_sequence, :bigint, null: false
       column :id, :uuid, unique: true, null: false, default: Sequel.lit('uuid_generate_v4()')
       column :aggregate_id, :uuid, null: false
       column :type, :varchar, null: false
       column :body, :jsonb, null: false
       column :metadata, :jsonb, null: false
 
-      index [:aggregate_id, :aggregate_sequence_id], unique: true
+      index [:aggregate_id, :aggregate_sequence], unique: true
     end
   end
 end
