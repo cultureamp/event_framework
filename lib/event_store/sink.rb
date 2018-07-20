b20b87f04856fb67d95fb9c1eeb9df027af57b82
Rob Jacoby <robert.jacoby@cultureamp.com>
2018-07-20 13:36:32 +1000

    Switch to using class << self syntax

--- lib/event_store/sink.rb
+++ lib/event_store/sink.rb
@@ -17,28 +17,31 @@ module EventFramework
         ['created_at', Sequel.lit("now() at time zone 'utc'")] + metadata.to_a.flatten.map(&:to_s)
       }
 
-      def self.sink(aggregate_id:, events:)
-        database.transaction do
-          events.each do |event|
-            begin
-              database[:events].insert(
-                aggregate_id: aggregate_id,
-                aggregate_sequence_id: event.aggregate_sequence_id,
-                type: event.class.name,
-                body: Sequel.pg_jsonb(EventBodySerializer.call(event)),
-                metadata: Sequel.function(:json_build_object, *MetadataSerializer.call(event.metadata)),
-              )
-            rescue Sequel::UniqueConstraintViolation
-              raise ConcurrencyError,
-                "error saving aggregate_id #{aggregate_id.inspect}, aggregate_sequence_id mismatch"
+      class << self
+        def sink(aggregate_id:, events:)
+          database.transaction do
+            events.each do |event|
+              begin
+                database[:events].insert(
+                  aggregate_id: aggregate_id,
+                  aggregate_sequence_id: event.aggregate_sequence_id,
+                  type: event.class.name,
+                  body: Sequel.pg_jsonb(EventBodySerializer.call(event)),
+                  metadata: Sequel.function(:json_build_object, *MetadataSerializer.call(event.metadata)),
+                )
+              rescue Sequel::UniqueConstraintViolation
+                raise ConcurrencyError,
+                  "error saving aggregate_id #{aggregate_id.inspect}, aggregate_sequence_id mismatch"
+              end
             end
           end
         end
-      end
 
-      private_class_method \
-      def self.database
-        EventStore.database
+        private
+
+        def database
+          EventStore.database
+        end
       end
     end
   end
