4df7b5cc9223cb70689dd122e7ae4b9378dd1c3a
Odin Dutton <odindutton@gmail.com>
2018-07-23 09:09:47 +1000

    Remove created_at from UnpersistedMetadata
    We shouldn't need to set the created_at timestamp as the event store
    will set it for us.

--- lib/event.rb
+++ lib/event.rb
@@ -4,8 +4,6 @@ module EventFramework
   class Event < Dry::Struct
     class UnpersistedMetadata < Dry::Struct
       transform_keys(&:to_sym)
-
-      attribute :created_at, Types::JSON::Time.meta(omittable: true)
     end
 
     class Metadata < Dry::Struct
