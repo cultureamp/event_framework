3c303b1d9a778931f9b4184183c4eff99223dfe5
Odin Dutton <odindutton@gmail.com>
2018-07-23 12:24:56 +1000

    Raise custom error when deserializing unknown event types

--- lib/event_store/source/event_type_deserializer.rb
+++ lib/event_store/source/event_type_deserializer.rb
@@ -2,8 +2,12 @@ module EventFramework
   module EventStore
     class Source
       class EventTypeDeserializer
+        UnknownEventType = Class.new(Error)
+
         def self.call(event_type)
           EventFramework.config.event_namespace_class.const_get(event_type)
+        rescue NameError
+          raise UnknownEventType, event_type
         end
       end
     end
--- spec/event_store/source/event_type_deserializer_spec.rb
+++ spec/event_store/source/event_type_deserializer_spec.rb
@@ -11,6 +11,13 @@ module EventFramework
             expect(EventTypeDeserializer.call('EventTypeDeserializerTested'))
               .to eq TestEvents::EventTypeDeserializerTested
           end
+
+          context 'with an unknown event type' do
+            it 'raises an error' do
+              expect { EventTypeDeserializer.call('BadEvent') }
+                .to raise_error EventTypeDeserializer::UnknownEventType, 'BadEvent'
+            end
+          end
         end
       end
     end
