68a70887016e1157f5cf1538b8c3ff668ca52c93
Odin Dutton <odindutton@gmail.com>
2018-07-23 11:00:21 +1000

    Fix expected/actual inconsistencies
    Actual goes on the left, expected on the right.

--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -1,14 +1,14 @@
 # NOTE: This does not check the metadata content.
 RSpec::Matchers.define :match_events do |expected_events|
   match do |actual_events|
-    expected_events.zip(actual_events).each do |expected_event, actual_event|
-      expect(expected_event.class).to eq actual_event.class
-      expect(expected_event.aggregate_id).to eq actual_event.aggregate_id
-      expect(expected_event.aggregate_sequence).to eq actual_event.aggregate_sequence
+    actual_events.zip(expected_events).each do |actual_event, expected_event|
+      expect(actual_event.class).to eq expected_event.class
+      expect(actual_event.aggregate_id).to eq expected_event.aggregate_id
+      expect(actual_event.aggregate_sequence).to eq expected_event.aggregate_sequence
 
       # body
-      expect(EventFramework::EventStore::Sink::EventBodySerializer.call(expected_event.to_h))
-        .to eq EventFramework::EventStore::Sink::EventBodySerializer.call(actual_event.to_h)
+      expect(EventFramework::EventStore::Sink::EventBodySerializer.call(actual_event.to_h))
+        .to eq EventFramework::EventStore::Sink::EventBodySerializer.call(expected_event.to_h)
 
       # metadata
       expect(actual_event.metadata).to be_a EventFramework::Event::Metadata
