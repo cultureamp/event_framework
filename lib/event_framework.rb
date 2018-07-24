fc1419307fd34c22c32af7823ac01ae1b8b646b2
Dan Cheail <dan.cheail@cultureamp.com>
2018-07-24 16:29:28 +1000

    replace Configuration with dry-configurable
    seems a nicer, easier-to-work-with syntax, and we get it for free considering it comes with dry-struct; makes sense to use?

--- lib/event_framework.rb
+++ lib/event_framework.rb
@@ -1,31 +1,25 @@
+require 'dry-configurable'
+
 module EventFramework
+  extend Dry::Configurable
+
   Error = Class.new(StandardError)
 
   autoload :Types, 'types'
   autoload :Event, 'event'
   autoload :EventStore, 'event_store'
 
-  class << self
-    attr_accessor :config
-
-    # See https://github.com/rails/rails/blob/20c91119903f70eb19aed33fe78417789dbf070f/railties/lib/rails.rb#L72
-    def environment
-      ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'
-    end
-
-    def configure
-      self.config ||= Configuration.new
-      yield config
-    end
+  # See https://github.com/rails/rails/blob/20c91119903f70eb19aed33fe78417789dbf070f/railties/lib/rails.rb#L72
+  def self.environment
+    ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'
   end
 
-  class Configuration
-    attr_accessor :database_url, :event_namespace_class
+  # the Module from which Event definitions are sourced
+  setting :event_namespace_class
 
-    def initialize
-      @event_namespace_class = Object
-    end
-  end
+  # the full URL used to connect to the database
+  # e.g. postgres://localhost/database_name
+  setting :database_url
 end
 
 # TODO: Move this config to domains/ as it's app specific
