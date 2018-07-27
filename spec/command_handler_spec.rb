94248e0bf1000659ea903b448499ce00629068cb
Dan Cheail <dan.cheail@cultureamp.com>
2018-07-27 12:57:21 +1000

    Refactor CommandHandler specs
    I split out the multiple expectations in `it handles events` into specific specs for the internal behaviour of `with_aggregate`
    
    Additionally, I added specs for how Metadata is initialised and handled within a CommandHandler

--- spec/command_handler_spec.rb
+++ spec/command_handler_spec.rb
@@ -1,45 +1,69 @@
 require 'spec_helper'
 
-module EventFramework
-  RSpec.describe CommandHandler do
-    let(:user_id) { SecureRandom.uuid }
-    let(:account_id) { SecureRandom.uuid }
-    let(:aggregate_id) { SecureRandom.uuid }
+RSpec.describe EventFramework::CommandHandler do
+  let(:user_id) { SecureRandom.uuid }
+  let(:account_id) { SecureRandom.uuid }
+  let(:aggregate_id) { SecureRandom.uuid }
 
-    describe '#with_aggregate' do
-      thing_command_handler = Class.new(described_class) do
-        def handle(aggregate_id:, command:)
-          with_aggregate(Thing, aggregate_id) do |thing|
-            thing.do_thing(foo: command.foo)
-          end
-        end
-      end
+  let(:metadata) do
+    double :metadata, user_id: user_id, account_id: account_id
+  end
 
-      it 'handles events' do
-        command = double :command, foo: 42
-        aggregate = double :aggregate
-        repository = double :repository
-        thing = class_double("Thing").as_stubbed_const
+  describe '#initialize' do
+    it 'has a required `metadata` argument' do
+      expect { described_class.new }
+        .to raise_error(ArgumentError, /metadata/)
+    end
+  end
 
-        allow(repository).to receive(:load_aggregate)
-          .with(Thing, aggregate_id)
-          .and_return(aggregate)
+  describe '#metadata' do
+    let(:instance) { described_class.new(metadata: metadata) }
 
-        expect(aggregate).to receive(:do_thing)
-          .with(foo: 42)
+    it 'is private' do
+      expect { instance.metadata }
+        .to raise_error(NoMethodError, /private method(.*)metadata/)
+    end
 
-        expect(repository).to receive(:save) do |actual_aggregate, metadata|
-          expect(actual_aggregate).to eq aggregate
-          expect(metadata.user_id).to eq user_id
-          expect(metadata.account_id).to eq account_id
-        end
+    it 'returns the metadata passed in via the initializer' do
+      expect(instance.send(:metadata)).to be metadata
+    end
+  end
 
-        handler = thing_command_handler.new(
-          user_id: user_id,
-          account_id: account_id,
-          repository: repository,
-        )
-        handler.handle(aggregate_id: aggregate_id, command: command)
+  describe '#with_aggregate' do
+    let(:aggregate)   { double :aggregate }
+    let(:repository)  { spy :repository, load_aggregate: aggregate }
+    let(:thing_class) { class_double "Thing" }
+
+    let(:instance) do
+      described_class.new(metadata: metadata, repository: repository)
+    end
+
+    let(:empty_block) { -> (aggregate) {} }
+
+    it 'is private' do
+      expect { instance.with_aggregate(thing_class, aggregate_id, &empty_block) }
+        .to raise_error(NoMethodError, /private method(.*)with_aggregate/)
+    end
+
+    it 'loads an aggregate from the repository' do
+      instance.send(:with_aggregate, thing_class, aggregate_id, &empty_block)
+
+      expect(repository).to have_received(:load_aggregate).with(thing_class, aggregate_id)
+    end
+
+    it 'yields the aggregate to a block' do
+      expect { |b| instance.send(:with_aggregate, thing_class, aggregate_id, &b) }
+        .to yield_with_args(aggregate)
+    end
+
+    # TODO: remove this
+    it 'saves the changes to the repository' do
+      instance.send(:with_aggregate, thing_class, aggregate_id, &empty_block)
+
+      expect(repository).to have_received(:save) do |actual_aggregate, metadata|
+        expect(actual_aggregate).to eq aggregate
+        expect(metadata.user_id).to eq user_id
+        expect(metadata.account_id).to eq account_id
       end
     end
   end
