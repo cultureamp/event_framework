70526c6b4991c4291e890958c38a67b6713ccad2
Dan Cheail <dan.cheail@cultureamp.com>
2018-07-27 12:57:37 +1000

    Implement metadata initializer for CommandHandler

--- lib/command_handler.rb
+++ lib/command_handler.rb
@@ -2,24 +2,22 @@ module EventFramework
   class CommandHandler
     attr_reader :repository
 
-    attr_reader :event_metadata
-
-    def initialize(user_id:, account_id:, repository: Repository)
+    def initialize(metadata:, repository: Repository)
       @repository = repository
-
-      @event_metadata = Metadata.new(
-        user_id: user_id,
-        account_id: account_id,
-      )
+      @metadata = metadata
     end
+
+    private
 
     def with_aggregate(aggregate_class, aggregate_id)
       aggregate = repository.load_aggregate(aggregate_class, aggregate_id)
 
       yield aggregate
 
-      repository.save aggregate, @event_metadata
+      repository.save aggregate, metadata
     end
+
+    attr_reader :metadata
 
     # TODO: with_new_aggregate
   end
