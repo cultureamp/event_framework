9c0b743130194184a0c039a696057cd1d46b31b9
Dan Cheail <dan.cheail@cultureamp.com>
2018-07-27 17:54:43 +1000

    Tweaks to EventFramework::Metadata
    Two things:
    
    * It shouldn’t contain created_at; that doesn’t exist on metadata until _after_ the event has been persisted; this class describes unpersisted metadata.
    
    * `to_h` shouldn’t include attributes that have nil-values, otherwise trying to use it when creating a dry-struct breaks, even if an attribute is marked as omittable.

--- lib/metadata.rb
+++ lib/metadata.rb
@@ -5,15 +5,11 @@ module EventFramework
     attr_accessor :user_id
     attr_accessor :account_id
 
-    attr_reader :created_at
-
     def initialize(**args)
       self.correlation_id = args[:correlation_id]
       self.causation_id = args[:causation_id]
       self.user_id = args[:user_id]
       self.account_id = args[:account_id]
-
-      @created_at = args[:created_at]
     end
 
     def to_h
@@ -22,8 +18,7 @@ module EventFramework
         causation_id: causation_id,
         user_id: user_id,
         account_id: account_id,
-        created_at: created_at,
-      }
+      }.delete_if { |_, v| v.nil? }
     end
   end
 end
