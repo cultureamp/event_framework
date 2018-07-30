086d4cae6fb204ab8fda1e56d7cd44f7d0e7f16b
Dan Cheail <dan.cheail@cultureamp.com>
2018-07-30 13:24:26 +1000

    Placating :rubocop:

--- spec/event_store/source/event_type_deserializer_spec.rb
+++ spec/event_store/source/event_type_deserializer_spec.rb
@@ -6,7 +6,6 @@ end
 
 OtherEventTypeDeserializerTested = Class.new(EventFramework::DomainEvent)
 
-
 module EventFramework
   module EventStore
     class Source
@@ -24,7 +23,7 @@ module EventFramework
             end
           end
 
-          context 'when called with a class name that exists otuside the declared parent domain' do |arg|
+          context 'when called with a class name that exists otuside the declared parent domain' do
             it 'raises an error' do
               expect { EventTypeDeserializer.call('OtherEventTypeDeserializerTested') }
                 .to raise_error EventTypeDeserializer::UnknownEventType, 'OtherEventTypeDeserializerTested'
