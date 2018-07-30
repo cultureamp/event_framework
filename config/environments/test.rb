5652edb99c58cead2b0df43ab65a1d8ab2283186
Dan Cheail <dan.cheail@cultureamp.com>
2018-07-30 18:10:08 +1000

    Look for `database_url` in ENV first

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,3 +1,3 @@
 EventFramework.configure do |config|
-  config.database_url = 'postgres://localhost/survey_event_store_test'
+  config.database_url = ENV.fetch('EVENT_STORE_DATABASE_URL', 'postgres://localhost/survey_event_store_test')
 end
