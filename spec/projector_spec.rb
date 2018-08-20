9b542f73badc77baa294950d9976a6c8b4f515c4
Odin Dutton <odindutton@gmail.com>
2018-08-20 11:03:10 +1000

    Extend projector specs

--- spec/projector_spec.rb
+++ spec/projector_spec.rb
@@ -1,15 +1,41 @@
 module EventFramework
   RSpec.describe Projector do
     describe '#process_events' do
-      let(:event) { instance_double(Event) }
+      let(:events) do
+        [
+          instance_double(Event, sequence: 2),
+          instance_double(Event, sequence: 1),
+        ]
+      end
+      let(:bookmark) { instance_double(Bookmark) }
+
+      subject(:projector) { described_class.new }
+
+      before do
+        allow(projector).to receive(:bookmark).and_return(bookmark)
+        allow(bookmark).to receive(:sequence=)
+        allow(projector).to receive(:handle_event)
+        allow(EventStore.database).to receive(:transaction).and_yield
+      end
 
       it 'opens a single database transaction for all events' do
         expect(EventStore.database).to receive(:transaction).once
 
-        described_class.new.process_events([event])
+        projector.process_events(events)
       end
 
-      it 'updates the bookmark'
+      it 'calls handle event for each event individually' do
+        expect(projector).to receive(:handle_event).with(events[0])
+        expect(projector).to receive(:handle_event).with(events[1])
+
+        projector.process_events(events)
+      end
+
+      it 'updates the bookmark once at the end' do
+        expect(bookmark).to receive(:sequence=).with(2)
+
+        projector.process_events(events)
+      end
     end
   end
 end
