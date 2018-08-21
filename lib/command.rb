535228956de425c8e230e88c93b9cd7399a13e06
Dan Cheail <dan.cheail@cultureamp.com>
2018-08-21 19:56:40 +1000

    Raise an error in Command if no schema exists

--- lib/command.rb
+++ lib/command.rb
@@ -3,6 +3,8 @@ require 'dry/validation'
 
 module EventFramework
   class Command < Dry::Struct
+    ValidationNotImplementedError = Class.new(Error)
+
     transform_keys(&:to_sym)
 
     attribute :aggregate_id, Types::UUID
@@ -13,6 +15,8 @@ module EventFramework
       end
 
       def validate(params)
+        raise ValidationNotImplementedError if @validation_schema.nil?
+
         @validation_schema.call(params)
       end
     end
--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -12,42 +12,58 @@ module EventFramework
       end
     end
 
-    let(:command_class) do
-      Class.new(Command) do
-        attribute :foo, Types::Strict::String
-        attribute :bar, Types::Strict::Integer
-
-        validation_schema do
-          required(:foo).filled(:str?)
-          required(:bar).filled(:int?)
-        end
-      end
-    end
-
     describe '.validate' do
-      context 'with valid input' do
-        it 'returns a successful result object' do
-          expect(command_class.validate('foo' => 'qux', bar: 42)).to be_success
+      context 'with no defined schema' do
+        let(:command_class) do
+          Class.new(Command) do
+            attribute :foo, Types::Strict::String
+            attribute :bar, Types::Strict::Integer
+          end
+        end
+
+        it 'raises an error' do
+          expect { command_class.validate({}) }.to raise_error(Command::ValidationNotImplementedError)
         end
       end
 
-      context 'with invalid input' do
-        let(:params) do
-          {
-            'foo' => Object.new,
-            bar: 'fourty two',
-          }
+      context 'with a defined schema' do
+        let(:command_class) do
+          Class.new(Command) do
+            attribute :foo, Types::Strict::String
+            attribute :bar, Types::Strict::Integer
+
+            validation_schema do
+              required(:foo).filled(:str?)
+              required(:bar).filled(:int?)
+            end
+          end
         end
 
-        it 'returns a failed result object' do
-          expect(command_class.validate(params)).to be_failure
+        context 'when given valid input' do
+          let(:params) do
+            { 'foo' => 'qux', bar: 42 }
+          end
+
+          it 'returns a successful result object' do
+            expect(command_class.validate(params)).to be_a_success
+          end
         end
 
-        it 'returns errors' do
-          expect(command_class.validate(params).errors).to eq(
-            foo: ['must be a string'],
-            bar: ['must be an integer'],
-          )
+        context 'when given invalid input' do
+          let(:params) do
+            { 'foo' => Object.new, bar: 'fourty two' }
+          end
+
+          it 'returns a failed result object' do
+            expect(command_class.validate(params)).to be_a_failure
+          end
+
+          it 'returns errors' do
+            expect(command_class.validate(params).errors).to eq(
+              foo: ['must be a string'],
+              bar: ['must be an integer'],
+            )
+          end
         end
       end
     end
