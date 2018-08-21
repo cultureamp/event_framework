18ec690c2f71430c966a2f8fb68414b745aed13a
Dan Cheail <dan.cheail@cultureamp.com>
2018-08-21 16:50:25 +1000

    Refactor build_handler into build_metadata
    90% of the old `build_handler` helper method was just a configurable
    way to build instances of Metadata; this cuts out the cruft and
    gets to the point.

--- lib/controller_helpers.rb
+++ lib/controller_helpers.rb
@@ -4,26 +4,26 @@ module EventFramework
   module ControllerHelpers
     extend Dry::Configurable
 
-    MissingRequestIdError = Class.new(StandardError)
-
     setting :metadata_class
     setting :user_id_resolver, -> {}
     setting :account_id_resolver, -> {}
     setting :request_id_resolver, -> {}
 
-    module Buildable
-      def build_handler(handler_class)
+    module MetadataHelper
+      MissingRequestIdError = Class.new(StandardError)
+
+      def build_metadata
         config = EventFramework::ControllerHelpers.config
 
-        metadata = config.metadata_class.new(
+        metadata_args = {
           user_id: instance_exec(&config.user_id_resolver),
           account_id: instance_exec(&config.account_id_resolver),
           correlation_id: instance_exec(&config.request_id_resolver),
-        )
+        }
 
-        raise MissingRequestIdError if metadata.correlation_id.nil?
-
-        handler_class.new(metadata: metadata)
+        config.metadata_class.new(**metadata_args).tap do |m|
+          raise MissingRequestIdError if m.correlation_id.nil?
+        end
       end
     end
   end
--- spec/controller_helpers_spec.rb
+++ spec/controller_helpers_spec.rb
@@ -10,17 +10,8 @@ module EventFramework
       end
     end
 
-    # every CommandHandler can be instantiated with a Metadata object
-    class StubHandler
-      attr_reader :metadata
-
-      def initialize(metadata:)
-        @metadata = metadata
-      end
-    end
-
     class StubController
-      include ControllerHelpers::Buildable
+      include ControllerHelpers::MetadataHelper
 
       # these methods are stubbed out by partial doubles in our specs
       def application_user_id; end
@@ -28,12 +19,10 @@ module EventFramework
       def application_account_id; end
 
       def params; end
-
-      def handler
-        build_handler StubHandler
-      end
     end
 
+    # `enable_test_interface`and `reset_config` are methods added by
+    # `dry/configurable` to make what we're doing really easy.
     before do
       described_class.enable_test_interface
       described_class.configure do |c|
@@ -47,16 +36,16 @@ module EventFramework
 
     let(:controller_instance) { StubController.new }
 
-    describe '#build_handler' do
+    describe 'MetadataHelper#build_metadata' do
       let(:user_id)    { SecureRandom.uuid }
       let(:account_id) { SecureRandom.uuid }
       let(:request_id) { SecureRandom.uuid }
 
-      let(:handler) { controller_instance.handler }
+      let(:metadata) { controller_instance.build_metadata }
 
       context 'if no request_id is available in the controller' do
         it 'raises an error' do
-          expect { handler }.to raise_error(described_class::MissingRequestIdError)
+          expect { metadata }.to raise_error(described_class::MetadataHelper::MissingRequestIdError)
         end
       end
 
@@ -73,16 +62,16 @@ module EventFramework
           allow(controller_instance).to receive(:params).and_return(request_id: request_id)
         end
 
-        it 'pre-seeds controller#user_id into metadata#user_id of a new command handler' do
-          expect(handler.metadata).to have_attributes(user_id: user_id)
+        it 'pre-seeds controller#user_id into the user_id attribute of a new metadata instance' do
+          expect(metadata).to have_attributes(user_id: user_id)
         end
 
-        it 'pre-seeds controller#account_id into metadata#account_id of a new command handler' do
-          expect(handler.metadata).to have_attributes(account_id: account_id)
+        it 'pre-seeds controller#account_id into the account_id attribute of a new metadata instance' do
+          expect(metadata).to have_attributes(account_id: account_id)
         end
 
-        it 'pre-seeds controller#request_id into metadata#correlation_id of a new command handler' do
-          expect(handler.metadata).to have_attributes(correlation_id: request_id)
+        it 'pre-seeds controller#request_id into the correlation_id attribute of a new metadata instance' do
+          expect(metadata).to have_attributes(correlation_id: request_id)
         end
       end
     end
