7d43b9c4ee245b025fe3de89ad68999c3be32d1b
Rafael Oliveira <rafael.oliveira@cultureamp.com>
2018-08-29 16:43:49 +1000

    first pass on creating new section command

--- db/structure.sql
+++ db/structure.sql
@@ -7,7 +7,6 @@ SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
@@ -40,6 +39,8 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
 COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
 
 
+SET search_path = public, pg_catalog;
+
 SET default_tablespace = '';
 
 SET default_with_oids = false;
@@ -48,10 +49,10 @@ SET default_with_oids = false;
 -- Name: events; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE public.events (
+CREATE TABLE events (
     sequence bigint NOT NULL,
     aggregate_sequence bigint NOT NULL,
-    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    id uuid DEFAULT uuid_generate_v4() NOT NULL,
     aggregate_id uuid NOT NULL,
     aggregate_type character varying(255) NOT NULL,
     event_type character varying(255) NOT NULL,
@@ -65,7 +66,7 @@ CREATE TABLE public.events (
 -- Name: events_sequence_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
-CREATE SEQUENCE public.events_sequence_seq
+CREATE SEQUENCE events_sequence_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -77,14 +78,14 @@ CREATE SEQUENCE public.events_sequence_seq
 -- Name: events_sequence_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
-ALTER SEQUENCE public.events_sequence_seq OWNED BY public.events.sequence;
+ALTER SEQUENCE events_sequence_seq OWNED BY events.sequence;
 
 
 --
 -- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE public.schema_migrations (
+CREATE TABLE schema_migrations (
     filename text NOT NULL
 );
 
@@ -93,14 +94,14 @@ CREATE TABLE public.schema_migrations (
 -- Name: events sequence; Type: DEFAULT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY public.events ALTER COLUMN sequence SET DEFAULT nextval('public.events_sequence_seq'::regclass);
+ALTER TABLE ONLY events ALTER COLUMN sequence SET DEFAULT nextval('events_sequence_seq'::regclass);
 
 
 --
 -- Name: events events_id_key; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY public.events
+ALTER TABLE ONLY events
     ADD CONSTRAINT events_id_key UNIQUE (id);
 
 
@@ -108,7 +109,7 @@ ALTER TABLE ONLY public.events
 -- Name: events events_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY public.events
+ALTER TABLE ONLY events
     ADD CONSTRAINT events_pkey PRIMARY KEY (sequence);
 
 
@@ -116,7 +117,7 @@ ALTER TABLE ONLY public.events
 -- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY public.schema_migrations
+ALTER TABLE ONLY schema_migrations
     ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (filename);
 
 
@@ -124,7 +125,7 @@ ALTER TABLE ONLY public.schema_migrations
 -- Name: events_aggregate_id_aggregate_sequence_index; Type: INDEX; Schema: public; Owner: -
 --
 
-CREATE UNIQUE INDEX events_aggregate_id_aggregate_sequence_index ON public.events USING btree (aggregate_id, aggregate_sequence);
+CREATE UNIQUE INDEX events_aggregate_id_aggregate_sequence_index ON events USING btree (aggregate_id, aggregate_sequence);
 
 
 --
