26daefc065113f3c86812b6755d11f7c4ffd0da8
Dan Cheail <dan.cheail@cultureamp.com>
2018-08-30 11:03:21 +1000

    Don't try to connect to RDS in Staging (yet)

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,3 +1,3 @@
-EventFramework.configure do |config|
-  config.database_url = EventFramework::ParameterStoreDatabaseConfiguration.new(ENV.fetch('STAGING_HOSTNAME')).database_url
-end
+# EventFramework.configure do |config|
+#   config.database_url = EventFramework::ParameterStoreDatabaseConfiguration.new(ENV.fetch('STAGING_HOSTNAME')).database_url
+# end
