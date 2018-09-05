6f80fbbca026da4ee41d74d0a7d9c21cb63ba3d7
Dan Cheail <dan.cheail@cultureamp.com>
2018-09-05 13:19:10 +1000

    Tidying event_framework.rb

--- lib/event_framework.rb
+++ lib/event_framework.rb
@@ -3,13 +3,12 @@ require 'dry/configurable'
 module EventFramework
   extend Dry::Configurable
 
+  # "Our" base-class for all framework-specific errors
   Error = Class.new(StandardError)
 
   # An exception raised within a currently-executing command that
   # can trigger a re-execution of the command.
   RetriableException = Class.new(Error)
-
-  AfterSinkHook = -> (events) {}
 
   autoload :Bookmark, 'bookmark'
   autoload :BookmarkRepository, 'bookmark_repository'
@@ -47,7 +46,7 @@ module EventFramework
 
   # An Object that responds to `.call` that is called with an array of new
   # Events any time an event or list of events is saved.
-  setting :after_sink_hook, AfterSinkHook
+  setting :after_sink_hook, -> (events) {}
 end
 
 require_relative "../config/environments/#{EventFramework.environment}"
