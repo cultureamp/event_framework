9a563076223817b3b8310eced4866b2fe16f3643
Odin Dutton <odindutton@gmail.com>
2018-10-15 14:47:58 +1100

    Add event processor lag monitor

---
+++ lib/event_framework/event_processor_monitor.rb
@@ -0,0 +1,48 @@
+module EventFramework
+  class EventProcessorMonitor
+    SLEEP_INTERVAL = 1
+
+    def initialize(
+      logger: Logger.new(STDOUT),
+      bookmark_readonly_class: BookmarkReadonly,
+      sequence_stats: EventStore::SequenceStats,
+      metrics:,
+      database: EventStore.database,
+      sleep_interval: SLEEP_INTERVAL
+    )
+      @logger = logger
+      @bookmark_readonly_class = bookmark_readonly_class
+      @sequence_stats = sequence_stats
+      @metrics = metrics
+      @database = database
+      @sleep_interval = sleep_interval
+    end
+
+    def call(processor_classes:)
+      loop do
+        processor_classes.each do |processor_class|
+          processor_lag = last_event_sequence(processor_class) - last_processed_event_sequence(processor_class)
+
+          logger.info "[#{processor_class.name}] #{processor_lag}"
+
+          metrics.emit_point('murmur.event_processor_lag', processor_lag, tags: ["processor_name:#{processor_class.name}"])
+        end
+
+        sleep sleep_interval
+      end
+    end
+
+    private
+
+    attr_reader :logger, :bookmark_readonly_class, :sequence_stats, :metrics, :database, :sleep_interval
+
+    def last_processed_event_sequence(processor_class)
+      bookmark_readonly_class.new(name: processor_class.name).sequence
+    end
+
+    def last_event_sequence(processor_class)
+      sequence_stats.refresh
+      sequence_stats.max_sequence(event_classes: processor_class.event_handlers.handled_event_classes)
+    end
+  end
+end
