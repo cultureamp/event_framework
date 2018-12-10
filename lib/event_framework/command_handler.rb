d6a7800793f132e23817468dd6b1cd40ce7f1277
Dan Cheail <dan.cheail@cultureamp.com>
2018-12-10 16:50:06 +1100

    Document CommandHandler in its current state
    In an attempt to "begin with why", I decided to document all the
    implicit behaviour that currently exists in CommandHandler. By doing
    this, I get another perspective on what each part of the base class
    does, which will help influence the changes I make.
    
    I used TomDoc (http://tomdoc.org) to structure the documentation.

--- lib/event_framework/command_handler.rb
+++ lib/event_framework/command_handler.rb
@@ -1,4 +1,6 @@
 module EventFramework
+  # Public: Encapsulates the process of executing a command against an aggregate
+  # within an event-sourced system.
   class CommandHandler
     extend Forwardable
 
@@ -8,11 +10,36 @@ module EventFramework
     MismatchedCommandError = Class.new(Error)
     RetryFailureThresholdExceededException = Class.new(Error)
 
+    # Public: Returns the instance of Repository used by `with_aggregate`
+    # and `with_new_aggregate`
     attr_reader :repository
 
     class << self
-      attr_reader :command_class, :callable
+      # Public: Returns the Proc that is executed on invocation of
+      # the CommandHandler
+      attr_reader :callable
 
+      # Public: Returns the Class that defines the structure of the data
+      # required by the CommandHandler
+      attr_reader :command_class
+
+      # Public: Define the behaviour to be executed on invocation of
+      # this CommandHandler.
+      #
+      # Each CommandHandler requires two components:
+      #
+      #  * a data object representing the input to the command
+      #  * a block that encapsulates command-specific behaviour
+      #
+      # Separating these concerns allows the `handle` method to focus on
+      # implementing the boiler-plate logic of argument validation, aggregate
+      # hydration, and retry-on-failure
+      #
+      # klass - The Class<Command> that defines the data structure expected by
+      #         the block
+      # block - The block to be executed on invocation
+      #
+      # Returns nothing.
       def handle(klass, &block)
         raise ArgumentError unless klass.is_a?(Class)
 
@@ -21,10 +48,30 @@ module EventFramework
       end
     end
 
+    # Public: Initializes a CommandHandler
+    #
+    # repository - A Repository, pre-configured with the required Event Sink and
+    # Event Store
     def initialize(repository: Repository.new)
       @repository = repository
     end
 
+    # Public: Invoke the CommandHandler
+    #
+    # command  - A Command, containing the data to be used during invocation.
+    #            Must be an instance of the class defined on the singleton class
+    #            via `CommandHander::handle`
+    # executor - An instance of Executor, representing the user invoking this
+    #            command.
+    # metadata - The Metadata pertaining to this specific command invocation.
+    #
+    # Returns nothing.
+    #
+    # Raises MismatchedCommandError if `command` is not an instance of the
+    # expected command class.
+    # Raises NotImplementedError if no handler has been defined.
+    # Raises RetryFailureThresholdExceededException if the number of automatic
+    # retries exceeds the designated threshold.
     def handle(command:, executor:, metadata:)
       raise NotImplementedError if command_class.nil? || callable.nil?
       raise MismatchedCommandError, "Received command of type #{command.class}; expected #{command_class}" unless command.is_a?(command_class)
@@ -48,11 +95,28 @@ module EventFramework
 
     private
 
+    # Make `command_class` and `callable` accessible as instance-level accessors
     def_delegators 'self.class', :command_class, :callable
+
     private :command_class, :callable
 
+    # Internal: the Metadata passed to `handle`
     attr_accessor :metadata
 
+    # Internal: Hydrates an instance of the given aggregate class by fetching
+    # a sequence of events from the Event Source, via a Repository.
+    #
+    # It then yields to the the handler, and persists any newly-generate events
+    # into the Event Sink (again, via the Repository)
+    #
+    # aggregate_class - the Class<Aggregate> to hydrate
+    # aggregate_id    - a String containing the UUID of the instance of
+    #                   aggregate_class to hydrate
+    #
+    # Yields an instance of the Class passed in via AggregateClass
+    #
+    # Raises Repository::AggregateNotFound if no events for the given
+    # aggregate_id exist in the Event Source
     def with_aggregate(aggregate_class, aggregate_id)
       aggregate = repository.load_aggregate(aggregate_class, aggregate_id)
 
@@ -61,6 +125,20 @@ module EventFramework
       repository.save_aggregate(aggregate, metadata: metadata)
     end
 
+    # Internal: Initializes an empty instance of the given aggregate class,
+    # ensuring no events currently exist for the given id, via a Repository
+    #
+    # It then yields to the the handler, and persists any newly-generate events
+    # into the Event Sink (again, via the Repository)
+    #
+    # aggregate_class - the Class<Aggregate> to hydrate
+    # aggregate_id    - a String containing the UUID of the instance of
+    #                   aggregate_class to hydrate
+    #
+    # Yields an instance of the Class passed in via AggregateClass
+    #
+    # Raises Repository::AggregateAlreadyExists if no events for the given
+    # aggregate_id exist in the Event Source
     def with_new_aggregate(aggregate_class, aggregate_id)
       aggregate = repository.new_aggregate(aggregate_class, aggregate_id)
 
