d777f52472c8b2c629efc8421ff77748ca36253e
Dan Cheail <dan.cheail@cultureamp.com>
2018-12-13 20:49:31 +1100

    Split ContextClassMethods into separate modules
    This helps document exactly what behaviour is being added to the context
    module.

--- lib/event_framework/context.rb
+++ lib/event_framework/context.rb
@@ -7,14 +7,14 @@ module EventFramework
     # Extends the given module with the behavior required for it to be used
     # as a Context
     def self.initialize_context(context_module, path_to_root)
-      context_module.extend ContextClassMethods
+      context_module.extend Environment
 
       context_module.define_singleton_method :root do
         Pathname.new(path_to_root).join('..')
       end
     end
 
-    module ContextClassMethods
+    module Environment
       # See https://github.com/rails/rails/blob/20c91119903f70eb19aed33fe78417789dbf070f/railties/lib/rails.rb#L72
       def environment
         ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'
