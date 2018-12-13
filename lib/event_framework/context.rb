d9c2dcea886b2250e2dee131df27e273954eaacd
Dan Cheail <dan.cheail@cultureamp.com>
2018-12-13 20:50:48 +1100

    Add behavior to enable registration of databases
    Establishes an IoC container within the module, defines a narrower
    interface to make it simper to define / retrieve databases from it.

--- lib/event_framework/context.rb
+++ lib/event_framework/context.rb
@@ -4,13 +4,20 @@ require 'sequel'
 
 module EventFramework
   module Context
+    NoSuchDatabaseRegisteredError = Class.new(Error)
+
     # Extends the given module with the behavior required for it to be used
     # as a Context
     def self.initialize_context(context_module, path_to_root)
       context_module.extend Environment
+      context_module.extend DatabaseRegistration
 
       context_module.define_singleton_method :root do
         Pathname.new(path_to_root).join('..')
+      end
+
+      context_module.define_singleton_method :container do
+        @container ||= Dry::Container.new
       end
     end
 
@@ -18,6 +25,20 @@ module EventFramework
       # See https://github.com/rails/rails/blob/20c91119903f70eb19aed33fe78417789dbf070f/railties/lib/rails.rb#L72
       def environment
         ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'
+      end
+    end
+
+    module DatabaseRegistration
+      def register_database(label)
+        container.namespace(:databases) do
+          register label.to_sym, DatabaseConnection.new(label.to_sym)
+        end
+      end
+
+      def database(label)
+        container.resolve("databases.#{label}")
+      rescue Dry::Container::Error
+        raise NoSuchDatabaseRegisteredError, "No database has been regisered for #{label}"
       end
     end
   end
