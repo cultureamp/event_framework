6ff0026b4a84a4689b6198bd3a523f3d7ffb1d19
Dan Cheail <dan.cheail@cultureamp.com>
2018-12-14 17:46:11 +1100

    Adding feedback to task runner

--- lib/event_framework/tasks/runner.rb
+++ lib/event_framework/tasks/runner.rb
@@ -4,6 +4,8 @@ require 'thor'
 module EventFramework
   module Tasks
     class Runner < Thor
+      include Thor::Actions
+
       def self.exit_on_failure?
         true
       end
@@ -15,8 +17,12 @@ module EventFramework
         connection = context_module(context_name).database(database_name.to_sym)
 
         DatabaseManager.new(connection).create
+
+        say_with_db context_name, database_name, "created"
+      rescue EventFramework::Context::NoSuchDatabaseRegisteredError
+        say_with_db context_name, database_name, "uknown database; has it been registered?", :red
       rescue DatabaseManager::DatabaseAlreadyExistsError
-        puts "#{context_name}/#{database_name} already exists, skipping"
+        say_with_db context_name, database_name, "already exists, skipping", :yellow
       end
 
       desc "drop_database CONTEXT DATABASE_NAME", "Drops the PostgreSQL database for the indicated context/database"
@@ -37,15 +43,21 @@ module EventFramework
           target_version: options[:version],
         )
 
+        say_with_db context_name, database_name, "migrated"
+
         invoke :dump_database_schema if !bypass_schema_dump && mod.environment == "development"
+      rescue Sequel::Migrator::Error => e
+        say_with_db context_name, database_name, "unable to migrate: #{e.message}", :red
       end
 
       desc "dump_database_schema CONTEXT DATABASE_NAME", "Dumps the curent SQL schema, for the indicated context/database"
       def dump_database_schema(context_name, database_name)
         mod = context_module(context_name)
         connection = mod.database(database_name.to_sym)
+        schema_path = mod.paths.db(database_name).join('structure.sql')
 
-        DatabaseManager.new(connection).dump_schema(schema_path: mod.paths.db(database_name).join('structure.sql'))
+        DatabaseManager.new(connection).dump_schema(schema_path: schema_path)
+        say_with_db context_name, database_name, "schema dumped to #{schema_path}"
       end
 
       desc "reset_database CONTEXT DATABASE_NAME", "Drops, creates and migrates the database for the indicated context/database"
@@ -59,7 +71,6 @@ module EventFramework
       def prepare_all
         EventFramework::Tasks.registered_contexts.each do |context_name|
           context_module(context_name).databases do |connection|
-            puts connection.label
             begin
               create_database(context_name, connection.label.to_s)
               migrate_database(context_name, connection.label.to_s, bypass_schema_dump: true)
@@ -69,6 +80,10 @@ module EventFramework
       end
 
       private
+
+      def say_with_db(context, db, message, color = nil)
+        say "[#{context}/#{db}] #{message}", color
+      end
 
       def context_module(name)
         inflector = Dry::Inflector.new
