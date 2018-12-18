2db8b01dda1404d294dd274587084bb27328966f
Dan Cheail <dan.cheail@cultureamp.com>
2018-12-19 10:51:58 +1100

    Restore "old" ParameterStoreDatabaseConfiguration
    We may as well leave this here, given the existing references to it

--- lib/event_framework/parameter_store_database_configuration.rb
+++ lib/event_framework/parameter_store_database_configuration.rb
@@ -1,6 +1,38 @@
-require_relative "../../../domains/lib/parameter_store_database_configuration"
+require 'aws-sdk-core'
+require 'aws-sdk-ssm'
 
 # TODO: Delete after configuration re-factor is complete
-module EventFramework
-  ParameterStoreDatabaseConfiguration = Domains::ParameterStoreDatabaseConfiguration
+module Domains
+  # Parameters are stored in the format /murmur/event_store/$FARM/database_url
+  class ParameterStoreDatabaseConfiguration
+    PARAMETER_PREFIX = '/murmur/event_store'
+
+    def initialize(farm_name)
+      @farm_name = farm_name
+    end
+
+    def database_url
+      ssm_client
+        .get_parameter(name: parameter_path, with_decryption: true)
+        .parameter
+        .value
+    rescue Aws::SSM::Errors::AccessDeniedException, Aws::SSM::Errors::ParameterNotFound
+      # If the instance can't connect to ParameterStore or the parameter, return nil;
+      # this will trigger a NotImplemented error if EventStore tries to connect,
+      # which we can handle gracefully up-stream.
+      nil
+    end
+
+    private
+
+    def ssm_client
+      @_ssm_client ||= Aws::SSM::Client.new(
+        region: 'us-west-2',
+      )
+    end
+
+    def parameter_path
+      [PARAMETER_PREFIX, @farm_name, 'database_url'].join('/')
+    end
+  end
 end
