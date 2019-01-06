d360f527e6cb8c259bd6f9597b10b9c937e0cd84
Dan Cheail <dan.cheail@cultureamp.com>
2019-01-07 10:53:15 +1100

    Refactor EventTypeSerializer for DI

--- lib/event_framework/event_store/event_type_serializer.rb
+++ lib/event_framework/event_store/event_type_serializer.rb
@@ -3,10 +3,18 @@ module EventFramework
     class EventTypeSerializer
       EventTypeDescription = Struct.new(:event_type, :aggregate_type)
 
-      def self.call(event_class)
-        parts = event_class.to_s.split('::') - EventFramework.config.event_namespace_class.to_s.split('::')
+      def initialize(event_container:)
+        @event_container = event_container
+      end
+
+      def call(event_class)
+        parts = event_class.name.split('::') - event_container.name.split('::')
         EventTypeDescription.new(*parts.reverse)
       end
+
+      private
+
+      attr_reader :event_container
     end
   end
 end
--- spec/event_store/event_type_serializer_spec.rb
+++ spec/event_store/event_type_serializer_spec.rb
@@ -7,9 +7,11 @@ end
 module EventFramework
   module EventStore
     RSpec.describe EventTypeSerializer do
-      describe '.call' do
+      subject(:event_type_serializer) { described_class.new(event_container: TestDomain) }
+
+      describe '#call' do
         it 'returns an event type description' do
-          expect(described_class.call(TestDomain::Thing::EventTypeSerializerTested))
+          expect(event_type_serializer.call(TestDomain::Thing::EventTypeSerializerTested))
             .to eq EventTypeSerializer::EventTypeDescription.new('EventTypeSerializerTested', 'Thing')
         end
       end
