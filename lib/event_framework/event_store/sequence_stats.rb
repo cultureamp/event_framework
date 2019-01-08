824d3dc9cd2d326cfb7801ab99e4ac5409a14028
Dan Cheail <dan.cheail@cultureamp.com>
2019-01-08 11:51:46 +1100

    Revert "Remove event class scoping from SequenceStats"
    This reverts commit aa0ffd0adadae2c8d1b8737e7489e3eaa32caa1d.

--- lib/event_framework/event_store/sequence_stats.rb
+++ lib/event_framework/event_store/sequence_stats.rb
@@ -1,17 +1,28 @@
 module EventFramework
   module EventStore
     class SequenceStats
-      def initialize(database:)
+      def initialize(database:, event_type_resolver:)
         @database = database
+        @event_type_resolver = event_type_resolver
       end
 
-      def max_sequence
-        database[:events_sequence_stats].max(:max_sequence).to_i
+      def max_sequence(event_classes: nil)
+        scope = database[:events_sequence_stats]
+
+        if event_classes
+          event_type_descriptions = event_classes.map { |event_type| event_type_resolver.serialize(event_type) }
+          scope = scope.where(
+            aggregate_type: event_type_descriptions.map(&:aggregate_type),
+            event_type: event_type_descriptions.map(&:event_type),
+          )
+        end
+
+        scope.max(:max_sequence).to_i
       end
 
       private
 
-      attr_reader :database
+      attr_reader :database, :event_type_resolver
     end
   end
 end
--- spec/event_store/sequence_stats_spec.rb
+++ spec/event_store/sequence_stats_spec.rb
@@ -20,13 +20,44 @@ module EventFramework
         insert_event sequence: 4, aggregate_type: 'B', event_type: 'A'
         insert_event sequence: 5, aggregate_type: 'B', event_type: 'A'
         insert_event sequence: 6, aggregate_type: 'B', event_type: 'C'
+
+        stub_const('TestDomain::A::A', Class.new)
+        stub_const('TestDomain::A::B', Class.new)
+        stub_const('TestDomain::B::A', Class.new)
+        stub_const('TestDomain::A::C', Class.new)
       end
 
-      subject(:sequence_stats) { described_class.new(database: EventFramework.test_database) }
+      let(:event_type_resolver) { EventTypeResolver.new(event_context_module: TestDomain) }
+
+      subject(:sequence_stats) do
+        described_class.new(
+          database: EventFramework.test_database,
+          event_type_resolver: event_type_resolver,
+        )
+      end
 
       describe '.max_sequence' do
-        it 'returns the max sequence in the event store' do
-          expect(sequence_stats.max_sequence).to eq 6
+        let(:event_classes) do
+          [
+            double(:event_class, name: 'TestDomain::A::A'),
+            double(:event_class, name: 'TestDomain::A::B'),
+          ]
+        end
+
+        it 'returns the max sequence for the aggregate and event type' do
+          expect(sequence_stats.max_sequence(event_classes: event_classes)).to eq 3
+        end
+
+        context 'when no rows are returned' do
+          let(:event_classes) do
+            [
+              double(:event_class, name: 'TestDomain::Z::Z'),
+            ]
+          end
+
+          it 'returns 0' do
+            expect(sequence_stats.max_sequence(event_classes: event_classes)).to eq 0
+          end
         end
       end
     end
