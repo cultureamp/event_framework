632bbb1ff2e8a0aa2307f7ab63a512cb51db518b
Dan Cheail <dan.cheail@cultureamp.com>
2019-05-09 15:19:42 +1000

    Remove EventStore.database from bookmark specs
    Have been replaced with injected references to the EventFramework
    test database.

--- spec/bookmark_readonly_spec.rb
+++ spec/bookmark_readonly_spec.rb
@@ -1,7 +1,7 @@
 module EventFramework
   RSpec.describe BookmarkReadonly do
-    let(:bookmarks_table) { EventStore.database[:bookmarks] }
-    let(:bookmark) { Bookmark.new(name: 'foo', bookmarks_table: bookmarks_table) }
+    let(:bookmarks_table) { EventFramework.test_database[:bookmarks] }
+    let(:bookmark) { described_class.new(name: 'foo', bookmarks_table: bookmarks_table) }
 
     before do
       bookmarks_table.insert(name: 'foo', sequence: 42)
--- spec/bookmark_repository_spec.rb
+++ spec/bookmark_repository_spec.rb
@@ -1,41 +1,45 @@
 module EventFramework
   RSpec.describe BookmarkRepository do
+    let(:database) { EventFramework.test_database }
+
+    subject(:bookmark_repository) { described_class.new(name: 'foo', database: database) }
+
     describe '#checkout' do
       context 'when the bookmark does not exist' do
         it 'returns a bookmark starting a 0' do
-          bookmark = described_class.new(name: 'foo').checkout
+          bookmark = bookmark_repository.checkout
 
           expect(bookmark.sequence).to eq 0
         end
 
         it 'inserts a new record into the database' do
-          described_class.new(name: 'foo').checkout
+          bookmark_repository.checkout
 
-          expect(EventStore.database[:bookmarks].all).to match [a_hash_including(name: 'foo', sequence: 0)]
+          expect(database[:bookmarks].all).to match [a_hash_including(name: 'foo', sequence: 0)]
         end
       end
 
       context 'when the bookmark exists' do
         before do
-          EventStore.database[:bookmarks].insert(name: 'foo', sequence: 42)
+          database[:bookmarks].insert(name: 'foo', sequence: 42)
         end
 
         it 'returns the bookmark' do
-          bookmark = described_class.new(name: 'foo').checkout
+          bookmark = bookmark_repository.checkout
 
           expect(bookmark.sequence).to eq 42
         end
 
         context 'when a lock is already taken' do
           before do
-            described_class.new(name: 'foo').checkout
+            bookmark_repository.checkout
           end
 
           it 'raises an error' do
-            lock_key = EventStore.database[:bookmarks].first[:lock_key]
+            lock_key = database[:bookmarks].first[:lock_key]
 
             # NOTE: Get a separate database connection
-            other_database_connection = Sequel.connect(EventFramework.config.database_url)
+            other_database_connection = Sequel.connect(RSpec.configuration.database_url)
             repository = described_class.new(name: 'foo', database: other_database_connection)
 
             expect { repository.checkout }
