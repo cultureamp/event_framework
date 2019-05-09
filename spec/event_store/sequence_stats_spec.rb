d325da515742b241a26f274eba049441c4be964d
Dan Cheail <dan.cheail@cultureamp.com>
2019-05-09 16:14:01 +1000

    Modify SequenceStats so it behaves as an instance
    This makes EventProcessorMonitor easier to configure

--- spec/event_store/sequence_stats_spec.rb
+++ spec/event_store/sequence_stats_spec.rb
@@ -1,51 +1,59 @@
-# module EventFramework
-#   module EventStore
-#     RSpec.describe SequenceStats do
-#       def insert_event(sequence:, aggregate_type:, event_type:)
-#         EventFramework.test_database[:events].overriding_system_value.insert(
-#           sequence: sequence,
-#           aggregate_id: SecureRandom.uuid,
-#           aggregate_sequence: 1,
-#           aggregate_type: aggregate_type,
-#           event_type: event_type,
-#           body: Sequel.pg_jsonb({}),
-#           metadata: Sequel.pg_jsonb({}),
-#         )
-#       end
+module EventFramework
+  module EventStore
+    RSpec.describe SequenceStats do
+      def insert_event(sequence:, aggregate_type:, event_type:)
+        EventFramework.test_database[:events].overriding_system_value.insert(
+          sequence: sequence,
+          aggregate_id: SecureRandom.uuid,
+          aggregate_sequence: 1,
+          aggregate_type: aggregate_type,
+          event_type: event_type,
+          body: Sequel.pg_jsonb({}),
+          metadata: Sequel.pg_jsonb({}),
+        )
+      end
 
-#       before do
-#         insert_event sequence: 1, aggregate_type: 'A', event_type: 'A'
-#         insert_event sequence: 2, aggregate_type: 'A', event_type: 'A'
-#         insert_event sequence: 3, aggregate_type: 'A', event_type: 'B'
-#         insert_event sequence: 4, aggregate_type: 'B', event_type: 'A'
-#         insert_event sequence: 5, aggregate_type: 'B', event_type: 'A'
-#         insert_event sequence: 6, aggregate_type: 'B', event_type: 'C'
-#       end
+      before do
+        insert_event sequence: 1, aggregate_type: 'A', event_type: 'A'
+        insert_event sequence: 2, aggregate_type: 'A', event_type: 'A'
+        insert_event sequence: 3, aggregate_type: 'A', event_type: 'B'
+        insert_event sequence: 4, aggregate_type: 'B', event_type: 'A'
+        insert_event sequence: 5, aggregate_type: 'B', event_type: 'A'
+        insert_event sequence: 6, aggregate_type: 'B', event_type: 'C'
+      end
 
-#       describe '.max_sequence' do
-#         let(:event_classes) do
-#           [
-#             double(:event_class, to_s: 'A::A'),
-#             double(:event_class, to_s: 'A::B'),
-#           ]
-#         end
+      subject(:sequence_stats) { described_class.new(database: EventFramework.test_database) }
 
-#         it 'returns the max sequence for the aggregate and event type' do
-#           expect(described_class.max_sequence(event_classes: event_classes)).to eq 3
-#         end
+      describe "#max_sequence" do
+        it 'returns the max sequence for all events in the database' do
+          expect(sequence_stats.max_sequence).to eq 6
+        end
+      end
 
-#         context 'when no rows are returned' do
-#           let(:event_classes) do
-#             [
-#               double(:event_class, to_s: 'Z::Z'),
-#             ]
-#           end
+      # describe '.max_sequence' do
+      #   let(:event_classes) do
+      #     [
+      #       double(:event_class, to_s: 'A::A'),
+      #       double(:event_class, to_s: 'A::B'),
+      #     ]
+      #   end
 
-#           it 'returns 0' do
-#             expect(described_class.max_sequence(event_classes: event_classes)).to eq 0
-#           end
-#         end
-#       end
-#     end
-#   end
-# end
+      #   it 'returns the max sequence for the aggregate and event type' do
+      #     expect(described_class.max_sequence(event_classes: event_classes)).to eq 3
+      #   end
+
+      #   context 'when no rows are returned' do
+      #     let(:event_classes) do
+      #       [
+      #         double(:event_class, to_s: 'Z::Z'),
+      #       ]
+      #     end
+
+      #     it 'returns 0' do
+      #       expect(described_class.max_sequence(event_classes: event_classes)).to eq 0
+      #     end
+      #   end
+      # end
+    end
+  end
+end
