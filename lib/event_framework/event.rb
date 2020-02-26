984bfdd6ec127cc0bfd96db0b753bceb29eab60e
Odin Dutton <odindutton@gmail.com>
2020-02-27 08:44:09 +1100

    Extract Event::Metadata into a separate class

--- lib/event_framework/event.rb
+++ lib/event_framework/event.rb
@@ -2,19 +2,21 @@ require 'dry/struct'
 
 module EventFramework
   class Event < DomainStruct
-    attribute :id, Types::UUID
-    attribute :sequence, Types::Strict::Integer
-    attribute :aggregate_id, Types::UUID
-    attribute :aggregate_sequence, Types::Strict::Integer
-    attribute :created_at, Types::JSON::Time
-
-    attribute :metadata, DomainStruct do
+    class Metadata < DomainStruct
       attribute :account_id, Types::UUID
       attribute :user_id, Types::UUID
       attribute :correlation_id, Types::UUID.meta(omittable: true)
       attribute :causation_id, Types::UUID.meta(omittable: true)
       attribute :migrated, Types::Bool.meta(omittable: true)
     end
+
+    attribute :id, Types::UUID
+    attribute :sequence, Types::Strict::Integer
+    attribute :aggregate_id, Types::UUID
+    attribute :aggregate_sequence, Types::Strict::Integer
+    attribute :created_at, Types::JSON::Time
+
+    attribute :metadata, Metadata
 
     attribute :domain_event, DomainEvent
 
